526923e2340540064c435fbc4c336a4baa9daace
omaralihussien55 <omaralihussien55@gmail.com>
2025-06-30 22:15:05 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-center p-2 mt-10 text-4xl font-extrabold">my store</h1>
+      <h1 className="text-center p-2 mt-10 text-4xl font-extrabold">my store by omar</h1>
     </div>
   );
 }
