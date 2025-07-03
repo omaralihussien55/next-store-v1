8f85fd690f2ae311b206ee733cb7093f5cbc3669
omaralihussien55 <omaralihussien55@gmail.com>
2025-07-03 22:50:35 +0300

    vbbnn

--- app/api/carts/[id]/route.ts
+++ app/api/carts/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 import { cart } from "../route";
 import { CartProduct } from "@/reactQuery/types";
 
