a0df0fdba1fa9ac9be1bdf8a12a3d76507106a44
omaralihussien55 <omaralihussien55@gmail.com>
2025-07-03 23:27:44 +0300

    bb

--- app/api/carts/[id]/route.ts
+++ app/api/carts/[id]/route.ts
@@ -1,38 +1,38 @@
-import {  NextRequest, NextResponse } from "next/server";
-import { cart } from "../route";
-import { CartProduct } from "@/reactQuery/types";
+// import {  NextRequest, NextResponse } from "next/server";
+// import { cart } from "../route";
+// import { CartProduct } from "@/reactQuery/types";
 
-export async function DELETE(
-    req: NextRequest, 
-    { params }: { params: { id: string } }
-) {
-  await req.json()
-    const productId = parseInt(params.id); // ✅ استخدم destructuring هنا
-  const cartRef = cart.carts[0];
+// export async function DELETE(
+//     req: NextRequest, 
+//     { params }: { params: { id: string } }
+// ) {
+  
+//     const productId = parseInt(params.id); // ✅ استخدم destructuring هنا
+//   const cartRef = cart.carts[0];
 
-  const productIndex = cartRef.products.findIndex(
-    (p: CartProduct) => p.id === productId
-  );
+//   const productIndex = cartRef.products.findIndex(
+//     (p: CartProduct) => p.id === productId
+//   );
 
-  if (productIndex === -1) {
-    return NextResponse.json(
-      { message: "Product not found in cart." },
-      { status: 404 }
-    );
-  }
+//   if (productIndex === -1) {
+//     return NextResponse.json(
+//       { message: "Product not found in cart." },
+//       { status: 404 }
+//     );
+//   }
 
-  cartRef.products.splice(productIndex, 1);
+//   cartRef.products.splice(productIndex, 1);
 
-  cartRef.total = cartRef.products.reduce((acc, p) => acc + p.total, 0);
-  cartRef.discountedTotal = cartRef.products.reduce(
-    (acc, p) => acc + p.discountedTotal,
-    0
-  );
-  cartRef.totalProducts = cartRef.products.length;
-  cartRef.totalQuantity = cartRef.products.reduce(
-    (acc, p) => acc + p.quantity,
-    0
-  );
+//   cartRef.total = cartRef.products.reduce((acc, p) => acc + p.total, 0);
+//   cartRef.discountedTotal = cartRef.products.reduce(
+//     (acc, p) => acc + p.discountedTotal,
+//     0
+//   );
+//   cartRef.totalProducts = cartRef.products.length;
+//   cartRef.totalQuantity = cartRef.products.reduce(
+//     (acc, p) => acc + p.quantity,
+//     0
+//   );
 
-  return NextResponse.json({ message: "Product removed from cart.", cart });
-}
+//   return NextResponse.json({ message: "Product removed from cart.", cart });
+// }
