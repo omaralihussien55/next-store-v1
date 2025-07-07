412c870121de44843d0686986306377d33e6a891
omaralihussien55 <omaralihussien55@gmail.com>
2025-07-07 21:57:58 +0300

    jj

---
+++ app/blog/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-white h-full p-4'>
+      <div className="grid-container">
+  <div className="box">1</div>
+  <div className="box">2</div>
+  <div className="box">3</div>
+</div>
+    </div>
+  )
+}
+
+export default page
